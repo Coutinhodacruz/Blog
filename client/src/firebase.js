242c3404c3dd208ac94bdf4f794996673a6af3ed
onukwufor dominic <dominicrotimi@gmail.com>
2024-01-29 22:51:50 -0500

    Blog page

---
+++ client/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB1ktAHSjRZW2WFl2aslMR7iVyE1McYkdY',
+  authDomain: 'my-blog-a4550.firebaseapp.com',
+  projectId: 'my-blog-a4550',
+  storageBucket: 'my-blog-a4550.appspot.com',
+  messagingSenderId: '699397991367',
+  appId: '1:699397991367:web:88ff565ef72a182d6b87e2',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
